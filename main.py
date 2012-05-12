4eda1f7182046beeaa56c54029e8bef810e2298b
scturtle <scturtle@gmail.com>
2012-05-12 16:44:00 +0800

    sync features from localhost version

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from bottle import static_file
 from markdown import markdown
 import os, stat, json
 
+markdown_options =['extra']
+
 @post('/convert')
 def convert():
-    return markdown(request.forms.get('content','').decode('utf-8'))
+    return markdown(request.forms.get('content','').decode('utf-8'), markdown_options)
 
 @route('/')
 def note():
